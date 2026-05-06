2f0b0b659a137d893565feddfbfaa7d0805531cc
miqbalputra <miqbalputra@users.noreply.github.com>
2026-05-07 01:32:36 +0700

    Fix: add GET /pendaftaran route so form is accessible at both / and /pendaftaran

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PendaftaranController::class, 'index']);
+Route::get('/pendaftaran', [PendaftaranController::class, 'index']);
 Route::post('/pendaftaran', [PendaftaranController::class, 'store']);
 Route::post('/upload-ocr', [PendaftaranController::class, 'uploadOcr'])->name('upload_ocr');
 Route::get('/pendaftaran/{id}/cetak', [PendaftaranController::class, 'cetak'])->name('pendaftaran.cetak');
